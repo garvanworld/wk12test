68a10ca7273b54725df2361995e4a4add08a0086
C T <20062443@tafe.wa.edu.au>
2024-10-10 11:55:15 +0800

    add UserAccount class to src_placeholder.py

--- src/src_placeholder.py
+++ src/src_placeholder.py
@@ -1,2 +1,21 @@
 x = 10
 print(f'var is {x}.')
+
+class UserAccount:
+
+    def __init__(self, firstname):
+        #constructor, store firstname
+        self.firstname = firstname
+
+    def __str__(self):
+        return f'In userAccount, firstname: {self.firstname}.'
+
+    def set_firstname(self, firstname):
+        self.firstname = firstname
+
+    def get_firstname(self):
+        return self.firstname
+
+peter = UserAccount("Pete firstn")
+print(f"created new UserAcct for {peter.get_firstname()}")
+print(peter)
